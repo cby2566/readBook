cc8e119ba1703d34fac033fe5029dfb1f1610bd3
cby2566 <870017005@qq.com>
2024-11-01 08:51:21 +0800

    project update

--- project/判断空.js
+++ project/判断空.js
@@ -18,5 +18,13 @@ export function isEmpty(val) {
 
   return false;
 }
+// 首字母大小
+export function titleCase(str) {
+  return str.replace(/( |^)[a-z]/g, L => L.toUpperCase());
+}
 
+// 下划转驼峰
+export function camelCase(str) {
+  return str.replace(/-[a-z]/g, str1 => str1.substr(-1).toUpperCase());
+}
 
